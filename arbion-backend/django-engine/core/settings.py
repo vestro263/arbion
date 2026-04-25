554393de4e5e4ef31d358f0c40768d8a3c907f2f
icontroller-jpg <walternyika07@gmail.com>
2026-04-25 14:12:26 +0530

    initial commit

--- arbion-backend/django-engine/core/settings.py
+++ arbion-backend/django-engine/core/settings.py
@@ -49,6 +49,8 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
 
+'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
 
@@ -107,9 +109,10 @@ USE_I18N = True
 USE_TZ = True
 
 
-# 📁 STATIC
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 AUTH_USER_MODEL = 'users.User'
 
