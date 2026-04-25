4519c941396770021f75858fb6c9d060232cf396
icontroller-jpg <walternyika07@gmail.com>
2026-04-25 14:00:00 +0530

    initial commit

--- arbion-ui/src/lib/config.js
+++ arbion-ui/src/lib/config.js
@@ -1,2 +1,2 @@
-export const DJANGO_HOST = 'http://127.0.0.1:8000'
+export const DJANGO_HOST = 'http://127.0.0.1:8000', 'https://arbion-jpg.onrender.com'
 export const NODE_HOST   = 'http://127.0.0.1:4000'
